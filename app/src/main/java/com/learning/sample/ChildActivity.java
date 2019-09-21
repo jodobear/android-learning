bc2f01462f4babc384070ec3b4bca5829fe3f93d
Ernest Asanov <ern.asanov@yandex.ru>
2019-09-21 21:02:42 +0300

    Classes 1 & 2

---
+++ app/src/main/java/com/learning/sample/ChildActivity.java
@@ -0,0 +1,24 @@
+package com.learning.sample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChildActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_child);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+}
--- app/src/main/java/com/learning/sample/MainActivity.java
+++ app/src/main/java/com/learning/sample/MainActivity.java
@@ -2,13 +2,58 @@ package com.learning.sample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView text;
+    private Button changeTextButton;
+    private Button startChildButton;
+
+    private boolean state = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text = findViewById(R.id.text);
+        changeTextButton = findViewById(R.id.textChangeButton);
+
+        changeTextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (state) {
+                    text.setText(R.string.first_text);
+                } else {
+                    text.setText(R.string.second_text);
+                }
+                state = !state;
+            }
+        });
+
+        startChildButton = findViewById(R.id.child_button);
+
+        startChildButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ChildActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
     }
 }
